bed6a7fac2045c0c36f05d9f2f48bb75a2971bd6
Oswaldo Rosales (AWF) <oswrosale2@ebay.com>
2024-10-06 17:37:01 -0600

    Initial commit

---
+++ src/main/java/com/posadev/api/StoplightDemoApplication.java
@@ -0,0 +1,13 @@
+package com.posadev.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoplightDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoplightDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/posadev/api/StoplightDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.posadev.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoplightDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
